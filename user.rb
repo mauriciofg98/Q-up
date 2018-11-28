5a8781b8b3d4e4d968c00658e1641e061284ff88
Gilbert <gibbyfonseca74@gmail.com>
2018-11-28 17:20:09 -0600

    failed

--- user.rb
+++ user.rb
@@ -3,13 +3,20 @@ require 'data_mapper' # metagem, requires common plugins too.
 class User
     include DataMapper::Resource
     property :id, Serial
-    property :email, String
-    property :password, String
     property :created_at, DateTime
+
+    property :haircut, Boolean, default => false
+    property :head_shave, Boolean, default => false
+    property :beard_trim, Boolean, default => false
+    property :beard_shave, Boolean, default => false
+
+    property :barber_choice, Serial
+
+    property :name, String
+
     property :administrator, Boolean, :default => false
 
     def login(password)
     	return self.password == password
     end
 end
-
